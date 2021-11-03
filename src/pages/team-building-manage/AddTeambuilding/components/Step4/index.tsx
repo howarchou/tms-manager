afe62c2bb3b4ddb16f744bf8c0d7cd835b5c814f
howarchou <howarchou@sina.com>
2021-11-03 23:26:39 +0800

    :zap:  add step4 profit

--- src/pages/team-building-manage/AddTeambuilding/components/Step4/index.tsx
+++ src/pages/team-building-manage/AddTeambuilding/components/Step4/index.tsx
@@ -10,7 +10,7 @@ import {
   InputNumber,
   Card,
   Descriptions,
-  Select,
+  Select
 } from 'antd';
 import type { Dispatch } from 'umi';
 import { connect } from 'umi';
@@ -23,7 +23,7 @@ interface Step4Props {
   submitting?: boolean;
 }
 
-const { TextArea } = Input;
+const {TextArea} = Input;
 const FormItemLayoutOffset = 0;
 
 const FileldsNameArray = {
@@ -38,25 +38,43 @@ const FileldsNameArray = {
   门票: '景区门票每人一张',
   场地: '拓展活动专用场地',
   住宿: '双人标间，含双早',
-  赠送: '活动气氛香槟、奖杯',
+  赠送: '活动气氛香槟、奖杯'
 };
 
+const PeopleNum: FC<{ num: number, onChange?: (num: number | string | undefined) => void }> = ({
+                                                                                                 num: nums,
+                                                                                                 onChange
+                                                                                               }) => {
+  return (
+    <Row align="middle">
+      <span style={{marginRight: 10}}>人数:</span>
+      <InputNumber
+        value={nums}
+        placeholder={'人数'}
+        onChange={onChange}
+      />
+    </Row>
+  );
+};
+
+
 const Step4: React.FC<Step4Props> = (props) => {
-  const { data, dispatch, submitting } = props;
+  const {data, dispatch, submitting} = props;
   const [form] = Form.useForm();
 
   useEffect(() => {
     form.setFieldsValue({
-      ...data,
+      ...data
     });
   }, [data]);
 
   const SCALE = 2;
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [num, setNum] = useState(1);
+  const [num, setNum] = useState<number>(1);
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [sum, setSum] = useState(0);
-  const { getFieldsValue, setFieldsValue } = form;
+  const [cost, setCost] = useState(0);
+  const {getFieldsValue, setFieldsValue} = form;
 
   const onPrev = () => {
     if (dispatch) {
@@ -65,12 +83,12 @@ const Step4: React.FC<Step4Props> = (props) => {
         type: 'addteambuilding/saveStepFormData',
         payload: {
           ...data,
-          ...values,
-        },
+          ...values
+        }
       });
       dispatch({
         type: 'addteambuilding/saveCurrentStep',
-        payload: 'schedule',
+        payload: 'schedule'
       });
     }
   };
@@ -80,22 +98,23 @@ const Step4: React.FC<Step4Props> = (props) => {
     if (dispatch) {
       dispatch({
         type: 'addteambuilding/saveStepFormData',
-        payload: values,
+        payload: values
       });
       dispatch({
         type: 'addteambuilding/saveCurrentStep',
-        payload: 'notice',
+        payload: 'notice'
       });
     }
   };
 
   const handleTotalPrice = () => {
     const value = form.getFieldsValue();
-    const { fee } = value;
+    const {fee} = value;
     let sumTemp = 0;
+    let countTemp = 0;
     if (fee && fee.length) {
       const calFee = fee.map(
-        (feeItem: { days: number; price: number; num: number; total_price: any }) => {
+        (feeItem: { days: number; price: number; num: number; total_price: any, cost_price: any }) => {
           if (feeItem === undefined) return feeItem;
           if (
             Number.isNaN(feeItem?.days) ||
@@ -106,34 +125,24 @@ const Step4: React.FC<Step4Props> = (props) => {
           }
           // eslint-disable-next-line no-param-reassign
           feeItem.total_price = feeItem.days * feeItem.price * feeItem.num;
+          countTemp += feeItem.days * feeItem.cost_price * feeItem.num;
           sumTemp += feeItem.total_price;
-          return { ...feeItem };
-        },
+          return {...feeItem};
+        }
       );
-      form.setFieldsValue({ fee: calFee });
+      form.setFieldsValue({fee: calFee});
     }
     setSum(sumTemp);
+    setCost(countTemp);
   };
 
-  const PeopleNum: FC<{ num: number }> = ({ num: nums }) => {
-    return (
-      <Row align="middle">
-        <span style={{ marginRight: 10 }}>人数:</span>
-        <InputNumber
-          value={nums}
-          placeholder={'人数'}
-          onChange={(value: number) => setNum(value)}
-        />
-      </Row>
-    );
-  };
 
   const handleChange = (value: string, index: number) => {
     const fileds = form.getFieldsValue();
-    const { fee } = fileds;
+    const {fee} = fileds;
     if (FileldsNameArray[value]) fee[index].intro = FileldsNameArray[value];
     setFieldsValue({
-      fee: [...fee],
+      fee: [...fee]
     });
   };
 
@@ -141,6 +150,11 @@ const Step4: React.FC<Step4Props> = (props) => {
     handleTotalPrice();
   }, [num]);
 
+  const handleChangeNum = (v: number | string | undefined) => {
+    if (typeof v === 'number' || typeof v === 'string') {
+      setNum(v as number);
+    }
+  };
   if (!data) {
     return null;
   }
@@ -148,27 +162,28 @@ const Step4: React.FC<Step4Props> = (props) => {
   return (
     <>
       <Form
-        style={{ height: '100%', marginTop: 40 }}
+        style={{height: '100%', marginTop: 40}}
         name={'plan'}
         form={form}
         layout="horizontal"
         autoComplete="off"
         initialValues={data}
       >
-        <Card style={{ marginBottom: 10 }}>
-          <Descriptions title={'统计信息'} column={4} extra={<PeopleNum num={num} />}>
+        <Card style={{marginBottom: 10}}>
+          <Descriptions title={'统计信息'} column={5} extra={<PeopleNum num={num as number} onChange={handleChangeNum}/>}>
             <Descriptions.Item label={'总计'}>{Number(sum).toFixed(SCALE)}</Descriptions.Item>
             <Descriptions.Item label={'含税'}>
               {Number(sum * Number(1 + 0.26)).toFixed(SCALE)}
             </Descriptions.Item>
             <Descriptions.Item label={'税率'}>{'6.00%'}</Descriptions.Item>
+            <Descriptions.Item label={'利润'}>{Number(sum - cost).toFixed(SCALE)}</Descriptions.Item>
             <Descriptions.Item label={'人均'}>
               {num ? Number(sum / num).toFixed(SCALE) : '0.00'}
             </Descriptions.Item>
           </Descriptions>
         </Card>
         <Form.List name="fee">
-          {(fields, { add, remove }) => (
+          {(fields, {add, remove}) => (
             <>
               {fields.map((field, fieldNum) => (
                 <Space
@@ -176,7 +191,7 @@ const Step4: React.FC<Step4Props> = (props) => {
                   style={{
                     display: 'flex',
                     marginTop: 20,
-                    marginBottom: 8,
+                    marginBottom: 8
                   }}
                   align="center"
                 >
@@ -185,7 +200,7 @@ const Step4: React.FC<Step4Props> = (props) => {
                     label={'项目:'}
                     name={[field.name, 'name']}
                     fieldKey={[field.fieldKey, 'name']}
-                    rules={[{ required: true, message: '请填写项目' }]}
+                    rules={[{required: true, message: '请填写项目'}]}
                   >
                     {/*<Input placeholder="项目"/>*/}
 
@@ -209,55 +224,55 @@ const Step4: React.FC<Step4Props> = (props) => {
                     label={'描述:'}
                     name={[field.name, 'intro']}
                     fieldKey={[field.fieldKey, 'intro']}
-                    rules={[{ required: true, message: '请填写描述' }]}
-                    style={{ width: 420 }}
+                    rules={[{required: true, message: '请填写描述'}]}
+                    style={{width: 420}}
                   >
-                    <Input placeholder="描述" />
+                    <Input placeholder="描述"/>
                   </Form.Item>
                   <Form.Item
                     {...field}
                     label={'优惠价:'}
                     name={[field.name, 'price']}
                     fieldKey={[field.fieldKey, 'price']}
-                    rules={[{ required: true, message: '请填写价格', type: 'number' }]}
+                    rules={[{required: true, message: '请填写价格', type: 'number'}]}
                   >
-                    <InputNumber placeholder="优惠价" onChange={handleTotalPrice} />
+                    <InputNumber placeholder="优惠价" onChange={handleTotalPrice}/>
                   </Form.Item>
                   <Form.Item
                     {...field}
                     label={'成本价:'}
                     name={[field.name, 'cost_price']}
                     fieldKey={[field.fieldKey, 'cost_price']}
-                    rules={[{ required: true, message: '请填写优惠价', type: 'number' }]}
+                    rules={[{required: true, message: '请填写优惠价', type: 'number'}]}
                   >
-                    <InputNumber placeholder="成本价" />
+                    <InputNumber placeholder="成本价"/>
                   </Form.Item>
                   <Form.Item
                     {...field}
                     label={'数量:'}
                     name={[field.name, 'num']}
                     fieldKey={[field.fieldKey, 'num']}
-                    rules={[{ required: true, message: '请填写数量', type: 'number' }]}
+                    rules={[{required: true, message: '请填写数量', type: 'number'}]}
                   >
-                    <InputNumber placeholder="数量" onChange={handleTotalPrice} />
+                    <InputNumber placeholder="数量" onChange={handleTotalPrice}/>
                   </Form.Item>
                   <Form.Item
                     {...field}
                     label={'天数:'}
                     name={[field.name, 'days']}
                     fieldKey={[field.fieldKey, 'days']}
-                    rules={[{ required: true, message: '请填写天数', type: 'number' }]}
+                    rules={[{required: true, message: '请填写天数', type: 'number'}]}
                   >
-                    <InputNumber placeholder="天数" onChange={handleTotalPrice} />
+                    <InputNumber placeholder="天数" onChange={handleTotalPrice}/>
                   </Form.Item>
                   <Form.Item
                     {...field}
                     label={'总价:'}
                     name={[field.name, 'total_price']}
                     fieldKey={[field.fieldKey, 'total_price']}
-                    rules={[{ required: true, message: '请填写总价', type: 'number' }]}
+                    rules={[{required: true, message: '请填写总价', type: 'number'}]}
                   >
-                    <InputNumber disabled={true} placeholder="总价" />
+                    <InputNumber disabled={true} placeholder="总价"/>
                   </Form.Item>
                   <MinusCircleOutlined
                     onClick={() => {
@@ -270,7 +285,7 @@ const Step4: React.FC<Step4Props> = (props) => {
               <Form.Item>
                 <Row justify="space-around" align={'middle'}>
                   <Col span={6}>
-                    <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                    <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined/>}>
                       添加项目
                     </Button>
                   </Col>
@@ -285,14 +300,14 @@ const Step4: React.FC<Step4Props> = (props) => {
               label="单价"
               name="price"
               rules={[
-                { required: true, message: '请输入单价' },
+                {required: true, message: '请输入单价'},
                 {
                   pattern: /^(\d+)((?:\.\d+)?)$/,
-                  message: '请输入合法金额数字',
-                },
+                  message: '请输入合法金额数字'
+                }
               ]}
             >
-              <InputNumber style={{ width: '100%' }} placeholder="单价" />
+              <InputNumber style={{width: '100%'}} placeholder="单价"/>
             </Form.Item>
           </Col>
         </Row>
@@ -301,9 +316,9 @@ const Step4: React.FC<Step4Props> = (props) => {
             <Form.Item
               label="不包含"
               name="cost_statement"
-              rules={[{ required: true, message: '请输入不包含说明' }]}
+              rules={[{required: true, message: '请输入不包含说明'}]}
             >
-              <TextArea placeholder="费用不包含说明" autoSize={{ minRows: 3, maxRows: 5 }} />
+              <TextArea placeholder="费用不包含说明" autoSize={{minRows: 3, maxRows: 5}}/>
             </Form.Item>
           </Col>
         </Row>
@@ -313,11 +328,11 @@ const Step4: React.FC<Step4Props> = (props) => {
             marginBottom: 20,
             display: 'flex',
             justifyContent: 'center',
-            flex: 1,
+            flex: 1
           }}
           align={'baseline'}
         >
-          <Button onClick={onPrev} style={{ marginRight: 8 }}>
+          <Button onClick={onPrev} style={{marginRight: 8}}>
             上一步
           </Button>
           <Button type="primary" onClick={onValidateForm} loading={submitting}>
@@ -330,15 +345,15 @@ const Step4: React.FC<Step4Props> = (props) => {
 };
 export default connect(
   ({
-    addteambuilding,
-    loading,
-  }: {
+     addteambuilding,
+     loading
+   }: {
     addteambuilding: StateType;
     loading: {
       effects: Record<string, boolean>;
     };
   }) => ({
     submitting: loading.effects['addteambuilding/submitStepForm'],
-    data: addteambuilding.step,
-  }),
+    data: addteambuilding.step
+  })
 )(Step4);
